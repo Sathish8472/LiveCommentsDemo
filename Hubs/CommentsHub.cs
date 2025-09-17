7aa351e90243654d0ab15ba64dba9ffb403a77c2
Sathish Selvam <sathish.selvam@totalmobile.co.uk>
2025-09-17 17:37:10 +0100

    Add project files.

---
+++ Hubs/CommentsHub.cs
@@ -0,0 +1,20 @@
+﻿using LiveCommentsDemo.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace LiveCommentsDemo.Hubs
+{
+    public class CommentsHub : Hub
+    {
+        public async Task SendComment(string user, string message)
+        {
+            var comment = new Comment
+            {
+                User = user,
+                Message = message,
+                Timestamp = DateTime.UtcNow
+            };
+
+            await Clients.All.SendAsync("ReceiveComment", comment);
+        }
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace LiveCommentsDemo.Models
+{
+    public class Comment
+    {
+        public string User { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
